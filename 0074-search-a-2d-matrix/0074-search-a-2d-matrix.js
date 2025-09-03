1a0b2a2602d4daa4f5c3d89ce3925f033becaba0
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-04 00:39:31 +0100

    Time: 0 ms (100%), Space: 53.8 MB (48.47%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let left = 0;
+    let right = m * n - 1;
+
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        let midValue = matrix[Math.floor(mid / n)][mid % n];
+
+        if(midValue === target) return true;
+        if(midValue < target){
+            left = mid + 1;
+        }else{
+            right = mid - 1
+        }
+    }
+
+    return false
+};
