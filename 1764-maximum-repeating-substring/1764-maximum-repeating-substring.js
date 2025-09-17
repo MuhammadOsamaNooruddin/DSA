a83279007451e9a336448b1bd64b20a27851e8f6
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-17 13:56:02 +0100

    Time: 1 ms (31.25%), Space: 55.2 MB (9.72%) - LeetHub

--- 1764-maximum-repeating-substring/1764-maximum-repeating-substring.js
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.js
@@ -4,25 +4,25 @@
  * @return {number}
  */
 var maxRepeating = function(sequence, word) {
-   const n = sequence.length;
-    const m = word.length;
+    
+    let m = sequence.length;
+    let n = word.length;
     let maxCount = 0;
 
-    for (let i = 0; i <= n - m; i++) {
+    for( let i= 0; i <= m-n; i++){
         let count = 0;
         let left = i;
-        let right = i + m;
+        let right = i + n;
 
-        // Count consecutive repetitions of 'word' starting at index i
-        while (sequence.slice(left, right) === word) {
+        while(sequence.slice(left,right) === word){
             count++;
-            left += m;
-            right += m;
+            left += n;
+            right += n;
         }
 
-        if (count > maxCount) maxCount = count;
+          if (count > maxCount) maxCount = count;
     }
 
-    return maxCount;
+    return maxCount
 
 };
