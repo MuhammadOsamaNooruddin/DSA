c8f6dc046ec38cf516494de7b12706c8b13d56af
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-24 21:43:20 +0100

    Time: 0 ms (100%), Space: 58.1 MB (5.42%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,17 +6,40 @@ var longestCommonPrefix = function(strs) {
    if(strs.length === 0) return ""
     if(strs.length === 1) return strs[0]
     
-    strs.sort();
-    let first = strs[0];
-    let last = strs[strs.length - 1];
+    // strs.sort();
+    // let first = strs[0];
+    // let last = strs[strs.length - 1];
+    // let res = "";
+
+    // for(let i=0; i<first.length; i++){
+    //     if(first[i] === last[i]){
+    //         res += first[i]
+    //     }else{
+    //         break;
+    //     }
+    // }
+
+    // return res
+
+    let minLengthStr = strs[0];
+    for(let s of strs){
+        if(minLengthStr.length > s.length){
+            minLengthStr = s
+        }
+    }
+
+    let i = 0;
     let res = "";
 
-    for(let i=0; i<first.length; i++){
-        if(first[i] === last[i]){
-            res += first[i]
-        }else{
-            break;
+    while(i < minLengthStr.length){
+        let char = strs[0][i];
+        for(let str of strs){
+            if(str[i] !== char){
+                return res;
+            }
         }
+        res += char
+        i++
     }
 
     return res
