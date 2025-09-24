8e7264770c81372ce045c9903fa91ac7a6576c90
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-24 19:39:33 +0100

    Time: 0 ms (100%), Space: 54.3 MB (79.52%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+
+    // if(!t.length) return false
+    if(s.length === "" && t.length === "") return true
+    if(s.length > t.length) return false;
+    
+    let pointer = 0;
+    let found = 0;
+
+    for(let i=0; i<t.length; i++){
+
+        if(s[pointer] === t[i]){
+            pointer++;
+            found++;
+        }
+
+    }
+
+    return found === s.length
+
+};
