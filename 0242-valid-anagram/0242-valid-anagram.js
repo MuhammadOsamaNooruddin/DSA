9bf6956629efe5d65c5bd356d26c327b7e97cf27
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-25 18:04:51 +0100

    Time: 19 ms (37.91%), Space: 61 MB (12.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+   
+   if(s.length !== t.length) return false;
+   if(s === "" || t === "") return false;
+
+    let myMap = new Map();
+
+    for(let n of s){
+        myMap.set(n, (myMap.get(n) || 0) + 1 )
+    }
+
+    for(let n of t){
+        if(myMap.has(n)){
+            myMap.set(n, myMap.get(n) - 1);
+            if(myMap.get(n) === 0 ) myMap.delete(n)
+        }
+    }
+
+    return myMap.size > 0 ? false : true
+};
