1872da8e0b751016927c7d530dec58607b627123
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-25 12:40:54 +0100

    Time: 0 ms (100%), Space: 55.9 MB (13.3%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -5,15 +5,21 @@
 var rotate = function(matrix) {
     let n = matrix.length;
 
-    // Step 1: Transpose the matrix
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+   for(let i=0; i< n; i++){
+        for(let j = i+1; j <n; j++){
+            [matrix[i][j],matrix[j][i]] = [matrix[j][i],matrix[i][j]]
         }
     }
 
-    // Step 2: Reverse each row
-    for (let i = 0; i < n; i++) {
-        matrix[i].reverse();
+    for(let i=0; i<n;i++){
+        let left = 0;
+        let right = n - 1;
+        while(left < right){
+            [matrix[i][left],matrix[i][right]] = [matrix[i][right],matrix[i][left]];
+            left++;
+            right--
+        }
     }
+
+
 };
