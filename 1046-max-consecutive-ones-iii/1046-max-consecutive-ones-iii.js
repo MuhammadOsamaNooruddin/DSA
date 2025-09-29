29b1fafc2df37bc45afcaa0ea86dac094d3a1dd7
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-29 14:44:45 +0100

    Time: 7 ms (10.93%), Space: 60.6 MB (86.86%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+
+    let maxOnes = 0;
+    let zeroCount = 0;
+    let left = 0;
+
+    for(let right=0; right<nums.length; right++){
+        if(nums[right] === 0){
+            zeroCount++;
+        }
+
+        while(zeroCount > k){
+            if(nums[left] === 0){
+                zeroCount--;
+            }
+            left++
+        }
+
+        maxOnes = Math.max(maxOnes, right - left + 1)
+
+    }
+    
+    return maxOnes;
+};
