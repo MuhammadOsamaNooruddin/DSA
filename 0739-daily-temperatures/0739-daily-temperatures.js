984fc8f88420139741a7d3f5192f513ce4180dfd
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-29 13:33:25 +0100

    Time: 20 ms (57.85%), Space: 78 MB (70.05%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    
+    let stack = [];
+    let res = new Array(temperatures.length).fill(0);
+    let j=0;
+
+    for(let i=0; i<temperatures.length; i++){
+       
+       while(stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]){
+            let index = stack.pop();
+            res[index] = i - index;
+       }
+
+       stack.push(i);
+
+    }
+
+    return res
+
+};
