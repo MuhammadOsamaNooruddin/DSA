b72d8cb8bd86c3bb4ea9b602445ff8b497bee00c
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-09-30 11:41:41 +0100

    Time: 111 ms (17.96%), Space: 63.3 MB (15.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -7,39 +7,60 @@ var checkInclusion = function(s1, s2) {
 
     if(s1.length > s2.length) return false;
     
-    let left = 0;
-    let right = 0;
-    let freqS1 = new Map();
-    let copyfreqS1 = new Map();
+    let freq = new Array(26).fill(0)
+    let window = new Array(26).fill(0)
 
-    for(let s of s1){
-        freqS1.set(s, (freqS1.get(s) || 0) + 1);
-        copyfreqS1.set(s, (copyfreqS1.get(s) || 0) + 1);
+    for(let c of s1){
+        freq[c.charCodeAt() - 97]++; 
     }
 
-    while(right < s2.length){
+    for(let i=0, j=0; i<s2.length; i++){
+        window[s2[i].charCodeAt() - 97]++;
 
-        if(right - left + 1 === s1.length){
-            let isTrue = helperFunction(s2.slice(left,right+1));
-            if(isTrue) return true;
-            else copyfreqS1 = new Map(freqS1);
-            left++;
+        if(i - j + 1 > s1.length){
+            window[s2[j++].charCodeAt() - 97]--;
         }
-        right++;
 
-    }
-
-
-    function helperFunction(a1){
-        for(let i=0; i< a1.length;i++){
-            if(copyfreqS1.has(a1[i])){
-                copyfreqS1.set(a1[i], copyfreqS1.get(a1[i]) - 1);
-                if(copyfreqS1.get(a1[i]) === 0) copyfreqS1.delete(a1[i])
-            }
+        if(freq.toString() === window.toString()){
+            return true
         }
-        
-        return copyfreqS1.size === 0;
-    }
 
+    }
     return false
+    // APPROACH 2 TIME COMPLEXITY O(n*m) SPACE COMPLEXITY O(n)
+    // let left = 0;
+    // let right = 0;
+    // let freqS1 = new Map();
+    // let copyfreqS1 = new Map();
+
+    // for(let s of s1){
+    //     freqS1.set(s, (freqS1.get(s) || 0) + 1);
+    //     copyfreqS1.set(s, (copyfreqS1.get(s) || 0) + 1);
+    // }
+
+    // while(right < s2.length){
+
+    //     if(right - left + 1 === s1.length){
+    //         let isTrue = helperFunction(s2.slice(left,right+1));
+    //         if(isTrue) return true;
+    //         else copyfreqS1 = new Map(freqS1);
+    //         left++;
+    //     }
+    //     right++;
+
+    // }
+
+
+    // function helperFunction(a1){
+    //     for(let i=0; i< a1.length;i++){
+    //         if(copyfreqS1.has(a1[i])){
+    //             copyfreqS1.set(a1[i], copyfreqS1.get(a1[i]) - 1);
+    //             if(copyfreqS1.get(a1[i]) === 0) copyfreqS1.delete(a1[i])
+    //         }
+    //     }
+        
+    //     return copyfreqS1.size === 0;
+    // }
+
+    // return false
 };
