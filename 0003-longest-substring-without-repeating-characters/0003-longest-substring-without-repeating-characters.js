e3b4f6128d354493c59ed89b98b8f832fc196635
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-01 23:06:01 +0100

    Time: 5 ms (79.85%), Space: 57.1 MB (77.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,28 +2,29 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function(str) {
 
-    if(!s.length) return 0;
-
-    let left = 0;
-    let right = 0;
-    let longest = 0;
-    let mySet = new Set();
-
-    while(right < s.length){
-
-        while(mySet.has(s[right])){
-            mySet.delete(s[left]);
-            left++
-        }
-
-        mySet.add(s[right]);
-        longest = Math.max(longest,right - left + 1);
-        right++
-
+     if(str.length === 0) return 0;
+  if(str.length === 1) return 1;
+  
+  let left = 0;
+  let right = 0;
+  let mySet = new Set();
+  let maxLength = 0;
+  
+  while(right < str.length){
+    
+    if(!mySet.has(str[right])){
+      mySet.add(str[right]);
+      right++
+    }else{
+      mySet.delete(str[left]);
+      left++
     }
-
-    return longest
+      maxLength = Math.max(maxLength, right - left);
+    
+  }
+  
+  return maxLength
 
 };
