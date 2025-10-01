cd984ec4c075ebf77598ed0f4b79dceac623b446
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-01 21:43:11 +0100

    Time: 3 ms (43.85%), Space: 63.2 MB (82.65%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+
+    let currSum = nums[0];
+    let maxSum = nums[0];
+    
+    for(let i=1; i <nums.length; i++){
+
+        currSum = Math.max(nums[i], nums[i] + currSum);
+        maxSum = Math.max(currSum, maxSum);
+
+    }
+
+    return maxSum
+};
