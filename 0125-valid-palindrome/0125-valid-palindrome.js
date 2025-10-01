9466ee3d389e63993741469f1995eb2c72a590ab
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-01 22:06:02 +0100

    Time: 3 ms (86.51%), Space: 58.3 MB (49.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,33 +2,25 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+var isPalindrome = function(str) {
 
-  let res = "";
+  let s = str.toLowerCase().replace(/[^a-z0-9]/g, "");
   
-  // Approach 1
-  //let letters = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
-  // Approach 1
+  // let newStr = "";
   
-  // Approach 2
-  let letters = []
-  for(let alp of s){
-    let char = alp.charCodeAt(0);
-    if((char >= 65 && char <= 90) || (char >= 97 && char <= 122) || (char >= 48 && char <= 57)){
-        letters.push(alp.toLowerCase());
-    }
+  // for(let i = s.length - 1; i >= 0; i--){
+  //   newStr += s[i];
+  // }
+  
+  let left = 0;
+  let right = s.length - 1;
+  
+  while(left < right){
+    if(s[left] !== s[right]) return false;
+    left++;
+    right--;
   }
-  //Approach 2
-
-    let left = 0;
-    let right = letters.length - 1;
-    
-    while(left < right){
-        if(letters[left] !== letters[right]) return false
-        left++;
-        right--;
-    }
-
-    return true
+  
+  return true
 
 };
