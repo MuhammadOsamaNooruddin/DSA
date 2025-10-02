694008475c99b650e119de156492e5c1e3d3dbf3
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-02 18:38:35 +0100

    Time: 9 ms (80.97%), Space: 60 MB (49.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -5,23 +5,55 @@
  */
 var topKFrequent = function(nums, k) {
 
+    // O(n) Approach
+
     let freq = {};
+    let bucket = [];
+    let result = [];
 
     for(let n of nums){
         freq[n] = (freq[n] || 0) + 1;
     }
 
-    let sorted = Object.entries(freq).sort((a,b) => b[1] - a[1]);
-
-    let res = [];
-    for(let [key, value] of sorted){
-        if(res.length !== k){
-            res.push(Number(key))
+    for( let [key,value] of Object.entries(freq)){
+        // console.log(key,value,bucket)
+        if(bucket[value]){
+            bucket[value].push(Number(key));
         }else{
-            break
+            bucket[value] = [Number(key)]
+        }
+
+    }
+    
+    for(let i=bucket.length - 1; i >= 0; i--){
+        if(result.length === k) break;
+        if(bucket[i]){
+            result.push(...bucket[i])
         }
     }
+    
+    return result
 
-    return res
+// -----------------------------------------
+    // O(n log n) Approach
+
+    // let freq = {};
+
+    // for(let n of nums){
+    //     freq[n] = (freq[n] || 0) + 1;
+    // }
+
+    // let sorted = Object.entries(freq).sort((a,b) => b[1] - a[1]);
+
+    // let res = [];
+    // for(let [key, value] of sorted){
+    //     if(res.length !== k){
+    //         res.push(Number(key))
+    //     }else{
+    //         break
+    //     }
+    // }
+
+    // return res
     
 };
