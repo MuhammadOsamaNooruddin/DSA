f09fd6419e74a80488b0ec3dabece4b875ee8be7
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-02 13:10:50 +0100

    Time: 0 ms (100%), Space: 54.3 MB (19.76%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -1,43 +1,29 @@
-/**
- * @param {number[][]} matrix
- * @return {number[]}
- */
 var spiralOrder = function(matrix) {
-    
-  let top = 0;
-  let bottom = matrix.length - 1;
-  let left = 0;
-  let right = matrix[0].length - 1;
-  let res = [];
+    const result = [];
+    let top = 0, bottom = matrix.length - 1;
+    let left = 0, right = matrix[0].length - 1;
 
-  while(top <= bottom && left <= right){
+    while (top <= bottom && left <= right) {
+        // Top row
+        for (let i = left; i <= right; i++) result.push(matrix[top][i]);
+        top++;
 
-    for(let i=left; i<=right; i++){
-        res.push(matrix[top][i])
-    }
-    top++;
+        // Right column
+        for (let i = top; i <= bottom; i++) result.push(matrix[i][right]);
+        right--;
 
-    for(let i=top; i<=bottom; i++){
-        res.push(matrix[i][right])
-    }
-    right--;
-
-    if(top <= bottom){
-        for(let i=right; i >= left; i--){
-            res.push(matrix[bottom][i])
+        // Bottom row (only if there’s a new row)
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) result.push(matrix[bottom][i]);
+            bottom--;
         }
-        bottom--;
-    }
 
-    if(left <= right){
-        for(let i=bottom; i>= top; i--){
-            res.push(matrix[i][left])
+        // Left column (only if there’s a new column)
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) result.push(matrix[i][left]);
+            left++;
         }
-        left++
     }
 
-  }
-
-  return res;
-    
+    return result;
 };
