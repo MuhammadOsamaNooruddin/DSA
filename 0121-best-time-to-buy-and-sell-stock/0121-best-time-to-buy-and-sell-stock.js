41f6d8dfa4a57621d58cbf2e30215e5503268db1
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-02 12:06:33 +0100

    Time: 3 ms (61.99%), Space: 65.4 MB (43.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+
     let buy = prices[0];
     let profit = 0;
 
-    for(let i = 1; i < prices.length; i++){
+    for(let i=1; i< prices.length; i++){
 
-        if(prices[i] < buy ){
-            buy = prices[i];
+        profit = Math.max(profit, prices[i] - buy);
+
+        if(buy > prices[i]){
+            buy = prices[i]
         }
-        profit = Math.max(profit, prices[i] - buy)
 
     }
 
     return profit
-
-};
+}
