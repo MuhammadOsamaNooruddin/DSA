ff4b6af93dd24ddeb17a3c08f14ec1633ecbd542
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-02 15:01:20 +0100

    Time: 35 ms (49.3%), Space: 66.5 MB (87.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,16 +4,38 @@
  */
 var groupAnagrams = function(strs) {
 
-    let sorted = strs.map((str) => str.split("").sort().join(""));
+    // let sorted = strs.map((str) => str.split("").sort().join(""));
+
+    // let map = {};
+
+    // for(let i=0; i < sorted.length; i++){
+    //     if(!map[sorted[i]]){
+    //         map[sorted[i]] = [strs[i]];
+    //     }else{
+    //         map[sorted[i]].push(strs[i])
+    //     }
+    // }
+
+    // return Object.values(map)
 
     let map = {};
 
-    for(let i=0; i < sorted.length; i++){
-        if(!map[sorted[i]]){
-            map[sorted[i]] = [strs[i]];
-        }else{
-            map[sorted[i]].push(strs[i])
+    for(let str of strs){
+
+        let count = new Array(26).fill(0);
+
+        for(let char of str){
+            count[char.charCodeAt(0) - 97]++;
         }
+
+        let key = count.join("#");
+
+        if(map[key]){
+            map[key].push(str);
+        }else{
+            map[key] = [str]
+        }
+        
     }
 
     return Object.values(map)
