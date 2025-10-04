1a9576e459fdbcbe008ff9bf58a89087ffccbd36
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-04 01:32:42 +0100

    Time: 3 ms (48.88%), Space: 68.4 MB (40.52%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -6,25 +6,27 @@
 var findMaxAverage = function(nums, k) {
 
     if(!nums.length) return 0;
+    if(k > nums.length) return 0;
+    if(nums.length === 1) return nums[0]
     
     let left = 0;
     let right = 0;
-    let res = -Infinity;
     let sum = 0;
+    let maxAvg = -Infinity;
 
     while(right < nums.length){
-        sum += nums[right];
-        if(right - left + 1 === k){
-            let avg = sum / k;
-            res = Math.max(res, avg);
-            sum -= nums[left];
-            left++;
-        }
 
+        sum += nums[right];
+
+        if(right - left  + 1 === k){
+            let avg = sum / k;
+            maxAvg = Math.max(maxAvg,avg);
+            sum -= nums[left];
+            left++
+        }
         right++
 
     }
 
-    return res
-
+    return maxAvg
 };
