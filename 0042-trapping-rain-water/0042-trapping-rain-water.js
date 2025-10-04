671a16ce133fc4458f6df0894e7dbe305019d9a1
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-04 01:25:02 +0100

    Time: 0 ms (100%), Space: 58.3 MB (44.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -6,28 +6,56 @@ var trap = function(height) {
 
     if(!height.length) return 0
     
-    let leftMax = [];
-    let rightMax = [];
-    let maxLeftCount = 0;
-    let maxRightCount = 0;
-    let count = 0;
+    // O(N) time and space
 
-    for(let i=0; i<height.length;i++){
-        leftMax[i] = maxLeftCount;
-        maxLeftCount = Math.max(maxLeftCount,height[i]);
+    // let leftMax = [];
+    // let rightMax = [];
+    // let leftMaxCounter = 0;
+    // let rightMaxCounter = 0;
+    // let count = 0;
+
+    // for(let i=0; i < height.length; i++){
+    //     leftMax[i] = leftMaxCounter;
+    //     leftMaxCounter = Math.max(leftMaxCounter, height[i]);
+    // }
+
+    // for(let i=height.length - 1; i>= 0; i--){
+    //     rightMax[i] = rightMaxCounter;
+    //     rightMaxCounter = Math.max(rightMaxCounter, height[i]);
+    // }
+
+    // for(let i=0; i < height.length; i++){
+    //     count += Math.max(0, Math.min(leftMax[i],rightMax[i]) - height[i] )
+    // }
+
+    // return count
+
+    // O(n) time and space O(1)
+
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let water = 0;
+
+    while(left <= right){
+
+        if(height[left] < height[right]){
+            if(height[left] > leftMax){
+                leftMax = height[left]
+            }else{
+                water += leftMax - height[left]
+            }
+            left++;
+        }else{
+            if(height[right] > rightMax){
+                rightMax = height[right]
+            }else{
+                water += rightMax - height[right]
+            }
+            right--
+        }
 
     }
-
-    for(let i=height.length - 1; i>=0;i--){
-        rightMax[i] = maxRightCount;
-        maxRightCount = Math.max(maxRightCount,height[i]);
-
-    }
-    
-    for(let i=0; i < height.length; i++){
-        count += Math.max(0, Math.min(rightMax[i], leftMax[i]) - height[i]);
-    }
-
-    return count
-
+    return water
 };
