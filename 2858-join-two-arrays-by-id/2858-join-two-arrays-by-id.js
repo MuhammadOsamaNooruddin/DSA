c155a0ac0838cd8229471913a378b5ba71670bf7
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-04 19:13:23 +0100

    Time: 297 ms (60.15%), Space: 99.1 MB (86.72%) - LeetHub

---
+++ 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+
+    let obj = {};
+    arr1.forEach((val) => obj[val.id] = val);
+    arr2.forEach((val) => obj[val.id] = { ...(obj[val.id] ?? {} ), ...val }) 
+
+    return Object.values(obj)
+};
