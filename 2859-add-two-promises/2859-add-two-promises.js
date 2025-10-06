74b15b0538db5408e6a375f0ee23f6c5c08cf118
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-06 19:15:28 +0100

    Time: 67 ms (5.78%), Space: 55 MB (21.52%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -5,10 +5,10 @@
  */
 var addTwoPromises = async function(promise1, promise2) {
     
-    // return Promise.all([promise1,promise2]).then(([val1,val2]) => val1 + val2)
-    let pro1 = await promise1;
-    let pro2 = await promise2;
-    return pro1 + pro2
+    return Promise.all([promise1,promise2]).then(([p1,p2]) => p1 + p2).catch((err) => err)
+    // let pro1 = await promise1;
+    // let pro2 = await promise2;
+    // return pro1 + pro2
 };
 
 /**
