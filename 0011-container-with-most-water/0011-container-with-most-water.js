99127937dd98891a435352261d49d30101d387fd
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-06 22:03:18 +0100

    Time: 2 ms (78.88%), Space: 63.8 MB (22.39%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -6,17 +6,18 @@ var maxArea = function(height) {
 
     let left = 0;
     let right = height.length - 1;
-    let maxArea = 0;
+    let maxArea = -Infinity;
 
     while(left < right){
-        let area = (right - left) * Math.min(height[left],height[right]);
-        maxArea = Math.max(maxArea, area);
+
+        let newArea = (right - left) * Math.min(height[left], height[right]);
+        maxArea = Math.max(maxArea, newArea)
+        
         if(height[left] < height[right]){
             left++
         }else{
             right--
         }
     }
-
     return maxArea
-};
+}
