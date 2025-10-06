c592672a4af804946466840ac90710cf831a1a42
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-06 21:06:43 +0100

    Time: 4 ms (55.52%), Space: 56.8 MB (26.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+
+    if(!nums.length) return [];
+
     let myMap = new Map();
 
-    for(let i = 0; i < nums.length; i++){
+    for(let i = 0; i <nums.length; i++){
         let diff = target - nums[i];
-
-        if(myMap.has(diff)){
-            return [i,myMap.get(diff)];
-        }
-            myMap.set(nums[i],i)
         
+        if(myMap.has(diff)){
+            return [i, myMap.get(diff)]
+        }else{
+            myMap.set(nums[i],i)
+        }
     }
-};
+
+    return []
+}
