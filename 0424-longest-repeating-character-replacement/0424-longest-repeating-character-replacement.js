183f3211c442d529f8c0fd2314f3606e114981bb
Muhammad Osama Noor Uddin <146275590+MuhammadOsamaNooruddin@users.noreply.github.com>
2025-10-07 00:46:59 +0100

    Time: 19 ms (66.03%), Space: 58.8 MB (28.69%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -5,31 +5,31 @@
  */
 var characterReplacement = function(s, k) {
 
-    if(!s) return 0;
-    if(s.length < k )return 0;
+    if(!s.length || s.length < k) return 0;
 
     let left = 0;
     let right = 0;
-    let longest = 0;
-    let myMap = new Map();
     let maxFreq = 0;
+    let seen = new Map();
+    let longest = 0;
 
     while(right < s.length){
 
-        let rightChar = s[right]; 
-        myMap.set(rightChar, (myMap.get(rightChar) || 0 ) + 1 );
+        let rightChar = s[right];
+        seen.set(rightChar, (seen.get(rightChar) || 0) + 1);
 
-        maxFreq = Math.max(maxFreq, myMap.get(rightChar));
+        maxFreq = Math.max(maxFreq,seen.get(rightChar));
 
         if((right - left + 1) - maxFreq > k){
             let leftChar = s[left];
-            myMap.set(leftChar, myMap.get(leftChar) - 1);
-            left++;
+            seen.set(leftChar, seen.get(leftChar) - 1);
+            if(seen.get(leftChar) === 0) seen.delete(leftChar);
+            left++
         }
-
+        
         longest = Math.max(longest, right - left + 1);
-        right++;
+        right++
+
     }
-   
     return longest
-};
+}
